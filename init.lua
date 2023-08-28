2689379868b933cb5760cf0720f10b611292c526
Adarsh Singh <adarsh@smoketrees.in>
2023-08-28 14:13:34 +0530

    feat: remove clutter

--- init.lua
+++ init.lua
@@ -35,11 +35,8 @@ vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
 
 vim.opt.list = true
-vim.opt.listchars:append("space:⋅")
-vim.opt.listchars:append("eol:↴")
 
 require("indent_blankline").setup({
-  space_char_blankline = " ",
   show_current_context = true,
   show_current_context_start = true,
   char_highlight_list = {
