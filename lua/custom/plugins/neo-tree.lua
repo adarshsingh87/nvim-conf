d180b3606d4c9ae70d1f2801b31d4fff8af565cb
Adarsh Singh <adarsh@smoketrees.in>
2024-06-12 12:14:23 +0530

    feat: NeoTree show dotfiles

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -7,9 +7,20 @@ return {
     'MunifTanjim/nui.nvim',
   },
   opts = {
-    close_if_last_window = false
+    close_if_last_window = false,
   },
   config = function()
+    require('neo-tree').setup({
+      close_if_last_window = false,
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false
+        }
+      }
+
+    })
     vim.cmd 'Neotree position=current toggle'
   end,
 }
