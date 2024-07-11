b1f1eab3c387a2c83b974b1f75e1c03414ff4f45
Adarsh Singh <adarsh@smoketrees.in>
2024-07-11 13:28:03 +0530

    feat: close the left file tree, and enable stateful toggle

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -23,7 +23,7 @@ return {
         {
           event = "file_opened",
           handler = function(file_path)
-            require("neo-tree.command").execute({ action = "close" })
+            require("neo-tree.command").execute({ action = "close", position = "left" })
           end
         },
       }
