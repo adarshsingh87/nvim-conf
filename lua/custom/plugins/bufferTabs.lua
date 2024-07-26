d9fec9211ca2066ae652a5ae111934757d9ba77b
Adarsh Singh <adarsh@smoketrees.in>
2024-07-26 12:29:39 +0530

    feat: buffer tabs with id

---
+++ lua/custom/plugins/bufferTabs.lua
@@ -0,0 +1,12 @@
+return {
+    'adarshsingh87/BufferTabs.nvim',
+    dependencies = {
+        'nvim-tree/nvim-web-devicons', -- optional
+    },
+    lazy = false,
+    config = function()
+        require('buffertabs').setup({
+            show_id = true
+        })
+    end
+}
